29d0250f6ab5c872b52f0b05ffa428a8655a32bc
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2025-06-17 10:59:04 +0530

    Update config.js

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     maxAge: 1000 * 60 * 60 * 24 * 2, // 2 days
 }
