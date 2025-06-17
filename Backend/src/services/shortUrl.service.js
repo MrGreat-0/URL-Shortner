bbb4614ee1ddb2ab9ab183cb6f6bc896186e0f03
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2025-06-17 11:05:58 +0530

    Update shortUrl.service.js

--- Backend/src/services/shortUrl.service.js
+++ Backend/src/services/shortUrl.service.js
@@ -2,20 +2,20 @@ import { generateNanoId } from "../utils/helper.js";
 import { getCustomShortUrl, saveShortUrl } from "../dao/shortUrl.js"
 
 export const createShortUrlServiceWithoutUser = async (originalUrl) => {
-    const shortUrl = generateNanoId(7);
+    const shortUrl = generateNanoId(6);
     if (!shortUrl) throw new Error("Short URL not generated");
     await saveShortUrl(originalUrl, shortUrl);
     return shortUrl;
     // console.log(url);
-    // res.send(nanoid(7));
+    // res.send(nanoid(6));
 }
 
 export const createShortUrlServiceWithUser = async (originalUrl, userId, slug = null) => {
-    const shortUrl = slug || generateNanoId(7);
+    const shortUrl = slug || generateNanoId(6);
     const exists = await getCustomShortUrl(slug);
     if (exists) throw new Error("This custom url already exists")
     await saveShortUrl(originalUrl, shortUrl, userId);
     return shortUrl;
     // console.log(url);
-    // res.send(nanoid(7));
+    // res.send(nanoid(6));
 }
