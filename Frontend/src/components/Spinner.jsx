4ca1b9de3fa18c41dd97b89398aead08b27012e7
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2025-06-19 01:38:47 +0530

    Create Spinner.jsx

---
+++ Frontend/src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="min-h-screen flex items-center justify-center bg-white">
+    <div className="animate-spin h-10 w-10 border-t-2 border-blue-500 rounded-full" />
+  </div>
+);
+
+export default Spinner;
